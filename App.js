9fe7bc392a446eb939f585248ff9f6eaec6515e9
unknown <b_rian_chiu@hotmail.com>
2018-10-20 23:58:27 -0400

    did one more thing

--- App.js
+++ App.js
@@ -1,30 +1,17 @@
 import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
-<<<<<<< HEAD
 import { createStackNavigator } from 'react-navigation';
 import Splash from './components/Splash'
 import Login from './components/Login'
-import Alarm from './components/alarms'
+import Alarm from './components/alarm<'
 import styles from './stylesheet'
-=======
-import Splash from './components/Splash';
-import Login from './components/Login';
-import styles from './stylesheet';
-import Alarm from './components/alarm';
->>>>>>> 203ead1c7dccd8980f9db5c583f936d700492278
 
 
 export default class App extends Component {
   
   render() {
-<<<<<<< HEAD
     return(
     <AppStack />
-=======
-    return (
-     //<Login />
-     <Alarm />
->>>>>>> 203ead1c7dccd8980f9db5c583f936d700492278
     );
   }
 
@@ -52,7 +39,7 @@ const AppStack = createStackNavigator({
     navigationOptions: {
       header: null,
     }
-  },
+  }<,
 
 });
 
