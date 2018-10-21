729c50f9c0bb6a9bd9e587024f7026099b4a6202
unknown <b_rian_chiu@hotmail.com>
2018-10-20 23:46:44 -0400

    react navigation attempt

--- App.js
+++ App.js
@@ -1,19 +1,48 @@
 import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
 import Splash from './components/Splash'
 import Login from './components/Login'
+import Alarm from './components/alarms'
 import styles from './stylesheet'
 
 
-//------------------------------splash
-
-
 export default class App extends Component {
+  
   render() {
-    return (
-     <Login />
+    return(
+    <AppStack />
     );
   }
+
 }
+
+
+const AppStack = createStackNavigator({
+  
+  Splash: { 
+    screen: Splash,
+    navigationOptions: {
+      header: null,
+    }
+  },
+
+  Login: {
+    screen: Login,
+    navigationOptions: {
+      header: null,
+    }
+  },
+
+  Alarm: {
+    screen: Alarm,
+    navigationOptions: {
+      header: null,
+    }
+  },
+
+});
+
+
 
 AppRegistry.registerComponent('heysun', () => App);
